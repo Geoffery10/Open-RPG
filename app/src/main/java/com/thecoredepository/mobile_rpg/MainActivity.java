4ea55dca83f7554c2214b5e8ca9eac9e06f45d94
Geoffery Powell <powellgeoffery@gmail.com>
2019-09-09 16:42:36 -0500

    Connected Main Activity to OL Sheet for Testing

--- app/src/main/java/com/thecoredepository/mobile_rpg/MainActivity.java
+++ app/src/main/java/com/thecoredepository/mobile_rpg/MainActivity.java
@@ -1,10 +1,15 @@
 package com.thecoredepository.mobile_rpg;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.mobile_rpg.R;
+import com.thecoredepository.mobile_rpg.charactersheets.openlegend.OLSheetActivity;
 import com.thecoredepository.mobile_rpg.charactersheets.openlegend.openlegend;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +18,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        Button btnOpenSheet = findViewById(R.id.btnOpenSheet);
+
+        btnOpenSheet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Starts Game
+                Intent in = new Intent(getApplicationContext(), OLSheetActivity.class);
+                startActivity(in);
+            }
+        });
     }
 }
