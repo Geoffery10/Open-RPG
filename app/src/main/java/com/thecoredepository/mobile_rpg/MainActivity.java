4fedf84311c42996af596acd25566e1e50dfb678
Geoffery Powell <powellgeoffery@gmail.com>
2019-09-09 16:33:33 -0500

    Moved Preview to Sheet Activity

--- app/src/main/java/com/thecoredepository/mobile_rpg/MainActivity.java
+++ app/src/main/java/com/thecoredepository/mobile_rpg/MainActivity.java
@@ -13,9 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        openlegend player = new openlegend();
-        player.generateRandomCharacter();
-        TextView textViewTest =findViewById(R.id.textTest);
-        textViewTest.setText(player.toString());
+        
     }
 }
--- app/src/main/java/com/thecoredepository/mobile_rpg/charactersheets/openlegend/OLSheetActivity.java
+++ app/src/main/java/com/thecoredepository/mobile_rpg/charactersheets/openlegend/OLSheetActivity.java
@@ -1,4 +1,20 @@
 package com.thecoredepository.mobile_rpg.charactersheets.openlegend;
 
-public class OLSheetActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.mobile_rpg.R;
+import com.thecoredepository.mobile_rpg.charactersheets.openlegend.openlegend;
+
+public class OLSheetActivity  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ol_sheet);
+        openlegend player = new openlegend();
+        player.generateRandomCharacter();
+        TextView textViewTest =findViewById(R.id.textTest);
+        textViewTest.setText(player.toString());
+    }
 }
