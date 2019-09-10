3a82f73dad1621fa2a2e677c26210e9ee13b9896
Geoffery Powell <powellgeoffery@gmail.com>
2019-09-09 23:07:45 -0500

    Started Feat Data Import

---
+++ app/src/main/java/com/thecoredepository/mobile_rpg/charactersheets/openlegend/featsOL.java
@@ -0,0 +1,32 @@
+package com.thecoredepository.mobile_rpg.charactersheets.openlegend;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class featsOL
+{
+    private String title;
+    private int level;
+    private int featCost;
+    private int featCostTotal;
+    private String description;
+    private String prerequisites;
+    private String effects;
+    private String special;
+
+    public void featList()
+    {
+        final static String fileName = "feats.txt";
+        featsOL[] list = new featsOL[53];
+
+        for (int i = 0; i <= 53; i++)
+        {
+
+        }
+    }
+}
