fae27c5705f2d18f8180c8c6c35f6f57fcc4042d
Geoffery Powell <powellgeoffery@gmail.com>
2019-09-10 00:25:00 -0500

    Created feats.json

--- app/src/main/java/com/thecoredepository/mobile_rpg/charactersheets/openlegend/featsOL.java
+++ app/src/main/java/com/thecoredepository/mobile_rpg/charactersheets/openlegend/featsOL.java
@@ -1,11 +1,16 @@
 package com.thecoredepository.mobile_rpg.charactersheets.openlegend;
 
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.os.Environment;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class featsOL
@@ -19,14 +24,27 @@ public class featsOL
     private String effects;
     private String special;
 
-    public void featList()
+    public void featList(Context context)
     {
-        final static String fileName = "feats.txt";
         featsOL[] list = new featsOL[53];
+        /*
+        String text = "";
+        try {
+            InputStream input = context.getAssets().open("feats.txt");
+            int size = input.available();
+            byte[] buffer = new byte[size];
+            input.read(buffer);
+            input.close();
+            text = new String(buffer);
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
 
         for (int i = 0; i <= 53; i++)
         {
 
         }
+
+         */
     }
 }
