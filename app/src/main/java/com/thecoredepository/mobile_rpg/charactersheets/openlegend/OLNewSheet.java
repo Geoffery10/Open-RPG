87655db169cc53c28d6150037e396c98fdef14f7
Geoffery Powell <powellgeoffery@gmail.com>
2019-09-17 11:39:57 -0500

    Completed Bio Section of New Character

--- app/src/main/java/com/thecoredepository/mobile_rpg/charactersheets/openlegend/OLNewSheet.java
+++ app/src/main/java/com/thecoredepository/mobile_rpg/charactersheets/openlegend/OLNewSheet.java
@@ -3,10 +3,12 @@ package com.thecoredepository.mobile_rpg.charactersheets.openlegend;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Layout;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -45,7 +47,8 @@ public class OLNewSheet extends AppCompatActivity {
                 EditText txtCharName = findViewById(R.id.txtCharName);
                 if (txtCharName.getText().toString().trim().length() > 0)
                 {
-                    player.setCharName(""+txtCharName.getText());
+                    getBioSection(txtCharName);
+
                     LinearLayout layoutBio = findViewById(R.id.layoutBio);
                     layoutBio.setVisibility(View.GONE);
                     LinearLayout layoutLang = findViewById(R.id.layoutLang);
@@ -117,5 +120,43 @@ public class OLNewSheet extends AppCompatActivity {
                 startActivity(in);
             }
         });
+    }
+
+    private void getBioSection(EditText txtCharName) {
+        player.setCharName(""+txtCharName.getText());
+        EditText txtNickName = findViewById(R.id.txtNickName);
+        player.setNickname(""+txtNickName.getText());
+        EditText txtDeity = findViewById(R.id.txtDeity);
+        if (txtDeity.getText().toString().trim().length() > 0) {
+            player.setDeity(""+txtDeity.getText());
+        }
+        else {
+            player.setDeity("No Deity");
+        }
+        getPlayerType();
+        EditText txtBioEdit = findViewById(R.id.txtBioEdit);
+        player.setBio(""+txtBioEdit.getText());
+    }
+
+    private void getPlayerType() {
+        Spinner spinnerType = findViewById(R.id.spinnerType);
+        String type = (String)spinnerType.getSelectedItem();
+        if (type.equals("Player Character")) {
+            type = "pc";
+        }
+        else if (type.equals("Companion Character - I")) {
+            type = "ccI";
+        }
+        else if (type.equals("Companion Character - II")) {
+            type = "ccII";
+        }
+        else if (type.equals("Companion Character - III")) {
+            type = "ccIII";
+        }
+        else {
+            Log.i("Type", "Error: "+type);
+            type = "pc";
+        }
+        player.setType(type);
     }
 }
