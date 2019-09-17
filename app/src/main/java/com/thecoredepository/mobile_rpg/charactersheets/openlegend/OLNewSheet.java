55411347972c60e399b164ea85627198df9f2276
Geoffery Powell <powellgeoffery@gmail.com>
2019-09-17 14:46:47 -0500

    Added +Levels and started -Buttons

--- app/src/main/java/com/thecoredepository/mobile_rpg/charactersheets/openlegend/OLNewSheet.java
+++ app/src/main/java/com/thecoredepository/mobile_rpg/charactersheets/openlegend/OLNewSheet.java
@@ -30,10 +30,14 @@ public class OLNewSheet extends AppCompatActivity {
         LinearLayout layoutBio = findViewById(R.id.layoutBio);
         layoutBio.setVisibility(View.VISIBLE);
 
-        //=========================THIS IS ONLY HERE UNTIL LEVELS ARE DONE!=========================
-        player.setLevelTotal(0);
-
         continueButtons();
+
+        final TextView editMajorLvl = findViewById(R.id.editMajorLvl);
+        final TextView editMinorLvl = findViewById(R.id.editMinorLvl);
+        final TextView editLevel = findViewById(R.id.editLevel);
+
+        plusLevelButtons(editMajorLvl, editMinorLvl, editLevel);
+        minusLevelButtons(editMajorLvl, editMinorLvl, editLevel);
 
         final TextView editAgility = findViewById(R.id.editAgility);
         final TextView editFortitude = findViewById(R.id.editFortitude);
@@ -59,9 +63,72 @@ public class OLNewSheet extends AppCompatActivity {
 
         final TextView editArmor = findViewById(R.id.editArmor);
 
-        plusButtons(editAgility, editFortitude, editMight, editLearning, editLogic, editPerception, editWill, editDeception, editPersuasion, editPresence, editAlteration, editCreation, editEnergy, editEntropy, editInfluence, editMovement, editPrescience, editProtection, editArmor);
+        plusAttributeButtons(editAgility, editFortitude, editMight, editLearning, editLogic, editPerception, editWill, editDeception, editPersuasion, editPresence, editAlteration, editCreation, editEnergy, editEntropy, editInfluence, editMovement, editPrescience, editProtection, editArmor);
+        minusAttributeButtons(editAgility, editFortitude, editMight, editLearning, editLogic, editPerception, editWill, editDeception, editPersuasion, editPresence, editAlteration, editCreation, editEnergy, editEntropy, editInfluence, editMovement, editPrescience, editProtection, editArmor);
 
         baneboonButtons();
+    }
+
+    private void plusLevelButtons(final TextView editMajorLvl, final TextView editMinorLvl, final TextView editLevel) {
+        ImageButton btnMajorLvlPlus = findViewById(R.id.btnMajorLvlPlus);
+        btnMajorLvlPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
+        ImageButton btnMinorLvlPlus = findViewById(R.id.btnMinorLvlPlus);
+        btnMinorLvlPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
+        ImageButton btnLevelPlus = findViewById(R.id.btnLevelPlus);
+        btnLevelPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                player.setLevelTotal(player.getLevelTotal() +  1);
+                editLevel.setText(""+player.getLevelTotal());
+                player.setLevel();
+                editMajorLvl.setText(""+player.getMajorLvl());
+                editMinorLvl.setText(""+player.getMinorLvl());
+            }
+        });
+    }
+
+    private void minusLevelButtons(final TextView editMajorLvl, final TextView editMinorLvl, final TextView editLevel) {
+        ImageButton btnMajorLvlPlus = findViewById(R.id.btnMajorLvlPlus);
+        btnMajorLvlPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
+        ImageButton btnMinorLvlPlus = findViewById(R.id.btnMinorLvlPlus);
+        btnMinorLvlPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
+        ImageButton btnLevelPlus = findViewById(R.id.btnLevelPlus);
+        btnLevelPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                player.setLevelTotal(player.getLevelTotal() +  1);
+                editLevel.setText(""+player.getLevelTotal());
+                player.setLevel();
+                editMajorLvl.setText(""+player.getMajorLvl());
+                editMinorLvl.setText(""+player.getMinorLvl());
+            }
+        });
     }
 
     private void baneboonButtons() {
@@ -83,7 +150,265 @@ public class OLNewSheet extends AppCompatActivity {
         });
     }
 
-    private void plusButtons(final TextView editAgility, final TextView editFortitude, final TextView editMight, final TextView editLearning, final TextView editLogic, final TextView editPerception, final TextView editWill, final TextView editDeception, final TextView editPersuasion, final TextView editPresence, final TextView editAlteration, final TextView editCreation, final TextView editEnergy, final TextView editEntropy, final TextView editInfluence, final TextView editMovement, final TextView editPrescience, final TextView editProtection, final TextView editArmor) {
+    private void plusAttributeButtons(final TextView editAgility, final TextView editFortitude, final TextView editMight, final TextView editLearning, final TextView editLogic, final TextView editPerception, final TextView editWill, final TextView editDeception, final TextView editPersuasion, final TextView editPresence, final TextView editAlteration, final TextView editCreation, final TextView editEnergy, final TextView editEntropy, final TextView editInfluence, final TextView editMovement, final TextView editPrescience, final TextView editProtection, final TextView editArmor) {
+
+        ImageButton btnAgilityPlus = findViewById(R.id.btnAgilityPlus);
+        ImageButton btnFortitudePlus = findViewById(R.id.btnFortitudePlus);
+        ImageButton btnMightPlus = findViewById(R.id.btnMightPlus);
+
+        ImageButton btnLearningPlus = findViewById(R.id.btnLearningPlus);
+        ImageButton btnLogicPlus = findViewById(R.id.btnLogicPlus);
+        ImageButton btnPerceptionPlus = findViewById(R.id.btnPerceptionPlus);
+        ImageButton btnWillPlus = findViewById(R.id.btnWillPlus);
+
+        ImageButton btnDeceptionPlus = findViewById(R.id.btnDeceptionPlus);
+        ImageButton btnPersuasionPlus = findViewById(R.id.btnPersuasionPlus);
+        ImageButton btnPresencePlus = findViewById(R.id.btnPresencePlus);
+
+        ImageButton btnAlterationPlus = findViewById(R.id.btnAlterationPlus);
+        ImageButton btnCreationPlus = findViewById(R.id.btnCreationPlus);
+        ImageButton btnEnergyPlus = findViewById(R.id.btnEnergyPlus);
+        ImageButton btnEntropyPlus = findViewById(R.id.btnEntropyPlus);
+        ImageButton btnInfluencePlus = findViewById(R.id.btnInfluencePlus);
+        ImageButton btnMovementPlus = findViewById(R.id.btnMovementPlus);
+        ImageButton btnPresciencePlus = findViewById(R.id.btnPresciencePlus);
+        ImageButton btnProtectionPlus = findViewById(R.id.btnProtectionPlus);
+
+        ImageButton btnArmorPlus = findViewById(R.id.btnArmorPlus);
+
+        btnAgilityPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                int maxScore = getMaxScore();
+                if ((player.getAgility() + 1) <= maxScore)
+                {
+                    player.setAgility(player.getAgility() + 1);
+                    editAgility.setText(""+player.getAgility());
+                    updateAttributes();
+                }
+            }
+        });
+        btnFortitudePlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                int maxScore = getMaxScore();
+                if ((player.getFortitude() + 1) <= maxScore) {
+                    player.setFortitude(player.getFortitude() + 1);
+                    editFortitude.setText("" + player.getFortitude());
+                    updateAttributes();
+                }
+            }
+        });
+        btnMightPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                int maxScore = getMaxScore();
+                if ((player.getMight() + 1) <= maxScore) {
+                    player.setMight(player.getMight() + 1);
+                    editMight.setText("" + player.getMight());
+                    updateAttributes();
+                }
+            }
+        });
+
+        btnLearningPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                int maxScore = getMaxScore();
+                if ((player.getLearning() + 1) <= maxScore) {
+                    player.setLearning(player.getLearning() + 1);
+                    editLearning.setText("" + player.getLearning());
+                    updateAttributes();
+                }
+            }
+        });
+        btnLogicPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                int maxScore = getMaxScore();
+                if ((player.getLogic() + 1) <= maxScore) {
+                    player.setLogic(player.getLogic() + 1);
+                    editLogic.setText("" + player.getLogic());
+                    updateAttributes();
+                }
+            }
+        });
+        btnPerceptionPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                int maxScore = getMaxScore();
+                if ((player.getPerception() + 1) <= maxScore) {
+                    player.setPerception(player.getPerception() + 1);
+                    editPerception.setText("" + player.getPerception());
+                    updateAttributes();
+                }
+            }
+        });
+        btnWillPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                int maxScore = getMaxScore();
+                if ((player.getWill() + 1) <= maxScore) {
+                    player.setWill(player.getWill() + 1);
+                    editWill.setText("" + player.getWill());
+                    updateAttributes();
+                }
+            }
+        });
+
+        btnDeceptionPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                int maxScore = getMaxScore();
+                if ((player.getDeception() + 1) <= maxScore) {
+                    player.setDeception(player.getDeception() + 1);
+                    editDeception.setText("" + player.getDeception());
+                    updateAttributes();
+                }
+            }
+        });
+        btnPersuasionPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                int maxScore = getMaxScore();
+                if ((player.getPersuasion() + 1) <= maxScore) {
+                    player.setPersuasion(player.getPersuasion() + 1);
+                    editPersuasion.setText("" + player.getPersuasion());
+                    updateAttributes();
+                }
+            }
+        });
+        btnPresencePlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                int maxScore = getMaxScore();
+                if ((player.getPresence() + 1) <= maxScore) {
+                    player.setPresence(player.getPresence() + 1);
+                    editPresence.setText("" + player.getPresence());
+                    updateAttributes();
+                }
+            }
+        });
+
+        btnAlterationPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                int maxScore = getMaxScore();
+                if ((player.getAlteration() + 1) <= maxScore) {
+                    player.setAlteration(player.getAlteration() + 1);
+                    editAlteration.setText("" + player.getAlteration());
+                    updateAttributes();
+                }
+            }
+        });
+        btnCreationPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                int maxScore = getMaxScore();
+                if ((player.getCreation() + 1) <= maxScore) {
+                    player.setCreation(player.getCreation() + 1);
+                    editCreation.setText("" + player.getCreation());
+                    updateAttributes();
+                }
+            }
+        });
+        btnEnergyPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                int maxScore = getMaxScore();
+                if ((player.getEnergy() + 1) <= maxScore) {
+                    player.setEnergy(player.getEnergy() + 1);
+                    editEnergy.setText("" + player.getEnergy());
+                    updateAttributes();
+                }
+            }
+        });
+        btnEntropyPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                int maxScore = getMaxScore();
+                if ((player.getEntropy() + 1) <= maxScore) {
+                    player.setEntropy(player.getEntropy() + 1);
+                    editEntropy.setText("" + player.getEntropy());
+                    updateAttributes();
+                }
+            }
+        });
+        btnInfluencePlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                int maxScore = getMaxScore();
+                if ((player.getInfluence() + 1) <= maxScore) {
+                    player.setInfluence(player.getInfluence() + 1);
+                    editInfluence.setText("" + player.getInfluence());
+                    updateAttributes();
+                }
+            }
+        });
+        btnMovementPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                int maxScore = getMaxScore();
+                if ((player.getMovement() + 1) <= maxScore) {
+                    player.setMovement(player.getMovement() + 1);
+                    editMovement.setText("" + player.getMovement());
+                    updateAttributes();
+                }
+            }
+        });
+        btnPresciencePlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                int maxScore = getMaxScore();
+                if ((player.getPrescience() + 1) <= maxScore) {
+                    player.setPrescience(player.getPrescience() + 1);
+                    editPrescience.setText("" + player.getPrescience());
+                    updateAttributes();
+                }
+            }
+        });
+        btnProtectionPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                int maxScore = getMaxScore();
+                if ((player.getProtection() + 1) <= maxScore) {
+                    player.setProtection(player.getProtection() + 1);
+                    editProtection.setText("" + player.getProtection());
+                    updateAttributes();
+                }
+            }
+        });
+
+        btnArmorPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                player.setArmor(player.getArmor() + 1);
+                editArmor.setText(""+player.getArmor());
+                updateAttributes();
+            }
+        });
+    }
+
+    private void minusAttributeButtons(final TextView editAgility, final TextView editFortitude, final TextView editMight, final TextView editLearning, final TextView editLogic, final TextView editPerception, final TextView editWill, final TextView editDeception, final TextView editPersuasion, final TextView editPresence, final TextView editAlteration, final TextView editCreation, final TextView editEnergy, final TextView editEntropy, final TextView editInfluence, final TextView editMovement, final TextView editPrescience, final TextView editProtection, final TextView editArmor) {
 
         ImageButton btnAgilityPlus = findViewById(R.id.btnAgilityPlus);
         ImageButton btnFortitudePlus = findViewById(R.id.btnFortitudePlus);
