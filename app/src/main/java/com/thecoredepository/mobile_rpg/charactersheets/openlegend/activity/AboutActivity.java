c3bee35c459b896c90d08659c7c22743eff402cf
Geoffery Powell <powellgeoffery@gmail.com>
2020-01-16 16:47:29 -0600

    Added Dev Icons

--- app/src/main/java/com/thecoredepository/mobile_rpg/charactersheets/openlegend/activity/AboutActivity.java
+++ app/src/main/java/com/thecoredepository/mobile_rpg/charactersheets/openlegend/activity/AboutActivity.java
@@ -1,8 +1,10 @@
 package com.thecoredepository.mobile_rpg.charactersheets.openlegend.activity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.view.Menu;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +12,11 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.thecoredepository.mobile_rpg.R;
 import com.thecoredepository.mobile_rpg.Theming;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class AboutActivity  extends AppCompatActivity
 {
@@ -26,5 +33,15 @@ public class AboutActivity  extends AppCompatActivity
         txtAboutHeader.setTextColor(Theming.getColoredFontColor());
         TextView txtAbout_01 = findViewById(R.id.txtAbout_01);
         txtAbout_01.setTextColor(Theming.getFontColor());
+
+        ImageView imgGeoffery = findViewById(R.id.imgGeoffery);
+        try {
+            Bitmap bitmap = BitmapFactory.decodeStream((InputStream)new URL("").getContent());
+            imgGeoffery.setImageBitmap(bitmap);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
