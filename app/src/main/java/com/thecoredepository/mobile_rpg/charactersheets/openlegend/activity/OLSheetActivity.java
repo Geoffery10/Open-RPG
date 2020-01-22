cb01e1c905df68465399bb16971b9e3005898012
Geoffery Powell <powellgeoffery@gmail.com>
2020-01-22 15:19:16 -0600

    Added Character Image Selection

--- app/src/main/java/com/thecoredepository/mobile_rpg/charactersheets/openlegend/activity/OLSheetActivity.java
+++ app/src/main/java/com/thecoredepository/mobile_rpg/charactersheets/openlegend/activity/OLSheetActivity.java
@@ -1,15 +1,22 @@
 package com.thecoredepository.mobile_rpg.charactersheets.openlegend.activity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,19 +27,27 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.bitmap.CircleCrop;
+import com.bumptech.glide.load.resource.bitmap.FitCenter;
 import com.thecoredepository.mobile_rpg.R;
 import com.thecoredepository.mobile_rpg.Theming;
 import com.thecoredepository.mobile_rpg.charactersheets.openlegend.OLEditSheet;
 import com.thecoredepository.mobile_rpg.dice.DiceActivity;
+
+import java.io.File;
+import java.io.InputStream;
 
 import static com.thecoredepository.mobile_rpg.charactersheets.openlegend.openlegend.player;
 import static com.thecoredepository.mobile_rpg.dice.dice.attributeToDice;
 
 public class OLSheetActivity extends AppCompatActivity {
 
+    private static final int GALLERY_REQUEST = 1000;
+    private static final int PERMISSION_CODE = 1001;
     //public openlegend player = new openlegend();
     public boolean bioEnabled = false;
     public Menu menu;
@@ -46,20 +61,59 @@ public class OLSheetActivity extends AppCompatActivity {
         player = player.loadCharacterSheet(selected);
         initializationOfElement();
         showHideBio();
+
+        //setCharacterImage();
+    }
+
+    private void setCharacterImage() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                requestPermissions(permissions, PERMISSION_CODE);
+            } else {
+                pickImageFromGallery();
+            }
+        } else {
+            pickImageFromGallery();
+        }
+    }
+
+    private void pickImageFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, GALLERY_REQUEST);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    pickImageFromGallery();
+                } else {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (resultCode == RESULT_OK && requestCode == GALLERY_REQUEST) {
+            ImageView imgTopSheet = findViewById(R.id.imgTopSheet);
+            Glide.with(this)
+                    .load(data.getData())
+                    .placeholder(R.drawable.banner_ol)
+                    .error(R.drawable.banner_ol)
+                    .skipMemoryCache(true)
+                    .diskCacheStrategy(DiskCacheStrategy.NONE)
+                    .transform(new FitCenter(), new CircleCrop())
+                    .into(imgTopSheet);
+        }
     }
 
     private void initializationOfElement() {
         //Initialization of Elements
-        ImageView imgTopSheet = findViewById(R.id.imgTopSheet);
-        Glide.with(this) //1
-                .load("$photoUrl?w=360")
-                .placeholder(R.drawable.banner_ol)
-                .error(R.drawable.banner_ol)
-                .skipMemoryCache(true) //2
-                .diskCacheStrategy(DiskCacheStrategy.NONE) //3
-                .into(imgTopSheet);
-
-
         CardView playerInfoCard = findViewById(R.id.playerInfoCard);
         playerInfoCard.setCardBackgroundColor(Color.parseColor(Theming.getCardViewBG()));
         CardView playerStatsCard = findViewById(R.id.playerStatsCard);
